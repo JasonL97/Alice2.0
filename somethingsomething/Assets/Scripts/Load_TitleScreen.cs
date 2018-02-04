813798dc692f021f544a212ee0467989a3568d88
ShaneLimJiXuan <33240428+ShaneLimJiXuan@users.noreply.github.com>
2018-02-04 11:51:02 +0800

    added gameOver & gameWin Screens

--- somethingsomething/Assets/Scripts/Load_TitleScreen.cs
+++ somethingsomething/Assets/Scripts/Load_TitleScreen.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Load_TitleScreen : MonoBehaviour {
+public class Load_TitleScreen : MonoBehaviour
+{
+    public Graphic fade;
+    public AudioSource sound;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public void Start()
+    {
 
+    }
     public void OnMouseButton()
     {
+        sound.Play();
+        StartCoroutine(DoFadeAndLoad());
+    }
+
+    private IEnumerator DoFadeAndLoad()
+    {
+        float alpha = 0;
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime;
+            Color color = fade.color;
+            color.a = alpha;
+            fade.color = color;
+            yield return new WaitForFixedUpdate();
+        }
         SceneManager.LoadScene("titleScreen");
+
     }
 }
